8f777a1e93ae5d531cf361d906fae5f8dd096956
iscas-zac <92451907+iscas-zac@users.noreply.github.com>
2022-10-27 12:50:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-slidev-library
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/slidev-g84ttp)
---
+++ dist/assets/DrawingPreview.74b9628a.js
@@ -0,0 +1,1 @@
+import"./index.287f0a76.js";import{ai as o}from"./index.287f0a76.js";export{o as default};
